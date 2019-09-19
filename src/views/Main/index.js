de36ceb5fddf513113d2607602e84e83053ad57b
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-09-19 17:40:25 -0300

    useless-import

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -3,7 +3,6 @@ import Header from "../../components/Header";
 import Body from "../../components/Body";
 import Footer from "../../components/Footer";
 import Root from "../../components/styles/Root";
-// import './main.css';
 
 const Main = () => (
   <Root>
