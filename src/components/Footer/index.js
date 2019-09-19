69a9bc8f9d77b33134e823820f2c9178b1a2f422
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-09-19 14:33:29 -0300

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Main } from "../styles/footer";
+
+const Footer = () => {
+  return (
+    <Main>
+      <h3>Testando suas habilidades em HTML, CSS e JS.</h3>
+      <h3>Linx Impulse</h3>
+      <h3>2019</h3>
+    </Main>
+  );
+};
+
+export default Footer;
---
+++ src/components/styles/footer.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  background: #707070 0% 0% no-repeat padding-box;
+  border: 1px solid #707070;
+  opacity: 1;
+  color: #ffffff;
+  text-align: center;
+  h3 {
+    flex: 1;
+    flex-basis: 100%;
+    margin-top: 1%;
+    margin-bottom: 1%;
+  }
+`;
