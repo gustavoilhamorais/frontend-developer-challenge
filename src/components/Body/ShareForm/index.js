b6b1ab322250b0f0d00f19b1d8cdce9d87b67544
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-09-19 16:45:04 -0300

    strong-email-validation

--- src/components/Body/ShareForm/index.js
+++ src/components/Body/ShareForm/index.js
@@ -18,14 +18,25 @@ const ShareForm = () => {
   };
 
   const submitData = () => {
-    window.alert('Show! Em instantes seu amigo terá acesso à uma lista exclusiva que enviaremos no email informado!');
+    window.alert(
+      `Show! Em instantes, ${name} receberá nosso link no email ${email}.`
+    );
   };
 
   const validateForm = () => {
-    const validateName = isNull(name) || isUndefined(name) || name === '';
-    const validateEmail = isNull(email) || isUndefined(email) || email === '';
-    if (!validateName && !validateEmail) submitData();
-    else window.alert('Você precisa informar um email e um nome válidos, por favor.');
+    const validateName = isNull(name) || isUndefined(name) || name === "";
+    const validateEmail = isNull(email) || isUndefined(email) || email === "";
+    if (!validateName && !validateEmail) emailStrongValidation(email);
+    else
+      window.alert(
+        "Você precisa informar um email e um nome válido, por favor."
+      );
+  };
+
+  const emailStrongValidation = email => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) submitData();
+    else alert("Por favor insira um endereço de email válido!");
+    return false;
   };
 
   return (
