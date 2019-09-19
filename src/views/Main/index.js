606f270344d955b29e5888066f2afbd19e649705
gustavoilhamorais <gustavoilhamorais@gmail.com>
2019-09-19 14:41:33 -0300

    page-main-component

---
+++ src/views/Main/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/Header";
+import Body from "../../components/Body";
+import Footer from "../../components/Footer";
+import Root from "../../components/styles/Root";
+// import './main.css';
+
+const Main = () => (
+  <Root>
+    <Header />
+    <Body />
+    <Footer />
+  </Root>
+);
+
+export default Main;
